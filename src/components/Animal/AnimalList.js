f258d22d1796a984c6a8ea96d9b011383ad26a13
Mark McCann <mjmccann12@me.com>
2020-04-22 10:43:30 -0500

    added logic to animallist

--- src/components/Animal/AnimalList.js
+++ src/components/Animal/AnimalList.js
@@ -1,17 +1,19 @@
 import React, { useContext } from "react"
 import { AnimalContext } from "../Animal/AnimalDataProvider"
+import { LocationContext } from "../Location/LocationDataProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
 import Animal from "../Animal/Animal"
 import "../Animal/Animal.css"
 
 
 export default () => {
     const { animals } = useContext(AnimalContext)
+    const { locations } = useContext(LocationContext)
+    const { customers } = useContext(CustomerContext)
 
     return (
         <div className="animals">
-            {
-                animals.map(dog => <Animal key={animal.id} animal={dog} />)
-            }
+            {animals.map(animal => <Animal key={animal.id} animal={animal} />)}
         </div>
     )
 }
