3795c5bffa9ac2e7362f3dec3be049dc3a1825e9
Mark McCann <mjmccann12@me.com>
2020-04-23 10:25:57 -0500

    modified dashboard and some imports

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,23 +2,19 @@ import React from "react"
 
 import "./Kennel.css"
 
-import Animal from "./Animal/Animal"
 import { AnimalDataProvider } from "./Animal/AnimalDataProvider"
 import AnimalList from "./Animal/AnimalList"
 import "./Animal/Animal.css"
 
 
-import Customer from "./Customer/Customer"
 import { CustomerDataProvider } from "./Customer/CustomerDataProvider"
 import CustomerList from "./Customer/CustomerList"
 import "./Customer/Customer.css"
 
-import Employee from "./Employee/Employee"
 import { EmployeeDataProvider } from "./Employee/EmployeeDataProvider"
 import EmployeeList from "./Employee/EmployeeList"
 import "./Employee/Employee.css"
 
-import Location from "./Location/Location"
 import LocationList from "./Location/LocationList"
 import LocationDataProvider from "./Location/LocationDataProvider"
 import "./Location/Location.css"
@@ -34,18 +30,18 @@ export default () => (
         </address>
 
 
-        <AnimalProvider>
-            <CustomerProvider>
-                <EmployeeProvider>
-                    <LocationProvider>
+        <AnimalDataProvider>
+            <CustomerDataProvider>
+                <EmployeeDataProvider>
+                    <LocationDataProvider>
                         <AnimalList />
                         <LocationList />
                         <CustomerList />
                         <EmployeeList />
-                    </LocationProvider>
-                </EmployeeProvider>
-            </CustomerProvider>
-        </AnimalProvider>
+                    </LocationDataProvider>
+                </EmployeeDataProvider>
+            </CustomerDataProvider>
+        </AnimalDataProvider>
     </>
 
 )
